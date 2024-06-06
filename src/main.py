f52bb9db6dd336b145187133e84afe5584947ac7
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-06-06 15:07:47 +0200

    [feat] Ajout de l'option de test dans le parseur

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 import modules
 import argparse
+from pathlib import Path
 
 class Parseur:
     def __init__(self):
         self.parser = argparse.ArgumentParser(usage='python3 alzette.py [OPTIONS]', add_help=False, description='Python script to launch a Jasmins\' implementation of Alzette')
         self.parse_arguments()
+
+    def check_test(self, option):
+        try:
+            int(option)
+            if Path("modules/tests/tests_bank/test" + option + ".py").exists():
+                return
+            else:
+                raise argparse.ArgumentTypeError("Error: your test hasn't been found.\n\tPlease make sure to use 'all' to run all tests or X where X is an integer corresponding to the associated test file in \n\tmodules/tests/tests_bank/testX.py\n\n")
+        except ValueError:
+            pass
+        if option == "all":
+            return option 
+        raise argparse.ArgumentTypeError("Error: your test hasn't been found.\n\tPlease make sure to use 'all' to run all tests or X where X is an integer corresponding to the associated test file in \n\tmodules/tests/tests_bank/testX.py\n\n")
 
     def parse_arguments(self):
         self.parser.add_argument('-h', '--help', action='help', help="shows this help message")
@@ -13,7 +27,7 @@ class Parseur:
         self.parser.add_argument('c', default = "0x98CBADFE", type=int, help='Value for c')
         self.parser.add_argument('-p', '--python_alzette', action='store_true', help="Launches Alzette (Python implementation)")
         self.parser.add_argument('-j', '--jasmin_alzette', action='store_true', help="Launches Alzette (Jasmin implementation)")
-        self.parser.add_argument('-t', '--test', action='store_true', help="Launches tests")
+        self.parser.add_argument('-t', '--test', type = self.check_test, help="Launches tests")
         self.parser.add_argument('-d', '--display', action='store_true', help="Display mode: displays results")
 
         self.args = self.parser.parse_args()
