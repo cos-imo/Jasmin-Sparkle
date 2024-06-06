c0b784a0c16bc738bea55369607c1586521bb330
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-06-06 11:53:10 +0200

    [feat] Lorsque la librairie n'est pas présente, on propose désormais à l'utilisateur de la compiler depuis le programme

--- src/modules/alzettejasmin/alzettejasmin.py
+++ src/modules/alzettejasmin/alzettejasmin.py
@@ -1,5 +1,8 @@
 import sys
 import ctypes
+import os
+import subprocess
+from pathlib import Path
 import numpy as np
 
 class AlzetteJasmin_t:
@@ -14,6 +17,18 @@ class AlzetteJasmin_t:
         self.load_library()
 
     def load_library(self):
+        if Path("modules/alzettejasmin/alzette.so").exists():
+            self.try_load()
+            return
+        else:
+            sys.stdout.write("Alzette library (.so) not found. Would you like to try to compile it using make? (y/n) >>>")
+            answer = input()
+            if answer in ["y", "Y", "yes", "Yes"]:
+                make = subprocess.run(["make", "-C", "modules/alzettejasmin/"], capture_output=True, text=True) 
+                sys.stdout.write(f" === RUNNING MAKE === \n stdout:\n{make.stdout}\n ===================== \n\n\n")
+                self.try_load()
+
+    def try_load(self):
         try:
             self.jasmin_alzette_dll = ctypes.cdll.LoadLibrary("./modules/alzettejasmin/alzette.so")
             sys.stdout.write("Alzette code successfully imported\n")
