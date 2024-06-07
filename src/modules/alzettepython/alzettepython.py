d939109a67c5ffaf85168b64e0b7de11a7a107d7
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-06-07 14:45:35 +0200

    [feat] L'implémentation Python se sert désormais d'une boucle appelant une fonction

--- src/modules/alzettepython/alzettepython.py
+++ src/modules/alzettepython/alzettepython.py
@@ -13,23 +13,18 @@ class AlzettePython_t:
         self.y = values[2]
 
     def alzette(self):
-        self.x += self.rotate_bits(self.y,31)
-        self.y ^= self.rotate_bits(self.x,24)
-        self.x ^= self.c
+        offset_1 = [31, 17, 0, 24]
+        offset_2 = [24, 17, 31, 16]
 
-        self.x += self.rotate_bits(self.y,17)
-        self.y ^= self.rotate_bits(self.x,17)
-        self.x ^= self.c
-
-        self.x += self.rotate_bits(self.y,0)
-        self.y ^= self.rotate_bits(self.x,31)
-        self.x ^= self.c
-
-        self.x += self.rotate_bits(self.y,24)
-        self.y ^= self.rotate_bits(self.x,16)
-        self.x ^= self.c
+        for i in range(0, 4):   
+            self.round(offset_1[i], offset_2[i])
 
         return self.x, self.y
+
+    def round(self, offset_1, offset_2):
+        self.x += self.rotate_bits(self.y, offset_1)
+        self.y ^= self.rotate_bits(self.x, offset_2)
+        self.x ^= self.c
 
     def rotate_bits(self, bits, offset):
         return (bits >> offset)|(bits << (32 - offset)) & 0xFFFFFFFF
