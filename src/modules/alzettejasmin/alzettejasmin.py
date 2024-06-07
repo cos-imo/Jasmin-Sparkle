b66f9a1906aa7013b1a5a74b98ab126ba2293448
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-06-07 14:13:39 +0200

    [fix] Remise en place du masque pour l'extraction de y

--- src/modules/alzettejasmin/alzettejasmin.py
+++ src/modules/alzettejasmin/alzettejasmin.py
@@ -48,6 +48,6 @@ class AlzetteJasmin_t:
         return_64 = self.jasmin_alzette_dll.alzette(padded_c, padded_x, padded_y)
 
         return_x = return_64 & 0xFFFFFFFF
-        return_y = (return_64 >> 32)
+        return_y = (return_64 >> 32) & 0xFFFFFFFF
 
         return return_x, return_y
