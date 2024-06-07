f907656cac0234a3392bf5ff81833dedaa7817f1
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-06-07 15:42:14 +0200

    [test] Les tests sont désormais exécutés sur le jeu de données fourni par le manager

--- src/modules/tests/testmanager.py
+++ src/modules/tests/testmanager.py
@@ -2,11 +2,13 @@ from tabulate import tabulate
 import modules.tests.tests_bank.test1
 
 class testmanager_t:
-    def __init__(self, choice="all", python_implementation = None, jasmin_implementation = None):
+    def __init__(self, choice="all", python_implementation = None, jasmin_implementation = None, values = [1,2,3]):
         self.test_library = [modules.tests.tests_bank.test1]
 
         self.python_implementation = python_implementation
         self.jasmin_implementation = jasmin_implementation
+
+        self.values = values
 
         self.run_test()
 
@@ -19,11 +21,10 @@ class testmanager_t:
     def run_test(self, test="all"):
         if test == "all":
             for i in range(len(self.test_library)):
-                test_result = self.test_library[i].test([1,2,3])
+                test_result = self.test_library[i].test(self.values)
                 print(test_result)
             return
         else:
-            test_result = self.test_library[test].test([1,2,3])
+            test_result = self.test_library[test].test(self.values)
             print("test don")
             print(test_result)
-
